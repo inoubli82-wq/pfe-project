0ea09ada73de28662a31d9520188b97a60744cc9
hamzus6 <safsafihamza3@gmail.com>
2026-04-05 02:18:23 +0100

    last

--- fix2.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('backend/controllers/partnerExportController.js', 'utf8');
-
-code = code.replace(/(\.json\(\{ success: false, message: "Action failed" \}))\s*\*\s*PUT/g, '\;\n  }\n};\n\n/**\n * PUT');
-
-fs.writeFileSync('backend/controllers/partnerExportController.js', code);
--- fix4.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('backend/controllers/partnerExportController.js', 'utf8');
-
-code = code.split('.json({ success: false, message: "Action failed" })\r\n * PUT').join('.json({ success: false, message: "Action failed" });\n  }\n};\n\n/**\n * PUT');
-
-code = code.split('.json({ success: false, message: "Action failed" })\n * PUT').join('.json({ success: false, message: "Action failed" });\n  }\n};\n\n/**\n * PUT');
-
-fs.writeFileSync('backend/controllers/partnerExportController.js', code);
